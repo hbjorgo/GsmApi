8eb232855b16a4f20acb829d95c4870dc471afb8
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2021-04-24 13:57:04 +0200

    Cleaned up CI build script

--- Build.ps1
+++ Build.ps1
@@ -1,16 +1,5 @@
-$artifacts = ".\artifacts"
-
-Write-Host "### Cleaning artifacts folder... ###"
-if(Test-Path $artifacts) { Remove-Item $artifacts -Force -Recurse }
-
 Write-Host "### dotnet clean... ###"
 dotnet clean .\src\HeboTech.GsmApi\HeboTech.GsmApi.csproj -c Release
 
 Write-Host "### dotnet build... ###"
 dotnet build .\src\HeboTech.GsmApi\HeboTech.GsmApi.csproj -c Release
-
-Write-Host "### dotnet test... ###"
-dotnet test .\src\HeboTech.GsmApi.Tests\HeboTech.GsmApi.Tests.csproj -c Release
-
-Write-Host "### dotnet pack... ###"
-dotnet pack .\src\HeboTech.GsmApi\HeboTech.GsmApi.csproj -c Release -o $artifacts --no-build
